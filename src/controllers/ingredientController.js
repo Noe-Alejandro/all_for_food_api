95a351854254541cc63ff99447d29811f7eb2765
EstebanMadrazo <3steban.madrazo@gmail.com>
2023-04-24 14:02:11 -0600

    Fix ingredient

--- src/controllers/ingredientController.js
+++ src/controllers/ingredientController.js
@@ -15,7 +15,7 @@ const postIngredient = (req, res) => {
         return ingredientService.postIngredient(body).then(ingredient => {
             res
                 .status(statusCode.Created)
-                .json(success("OK", ingredient, statusCode.OK));
+                .json(success("Created", ingredient, statusCode.OK));
         });
     } catch (e) {
         HandlerException(e, res)
@@ -26,7 +26,7 @@ const getAllIngredient = (req, res) => {
     try {
         var pagination = GetConfigPagination(req);
 
-        return recipeService.getAllIngredient(pagination).then(ingredients => {
+        return ingredientService.getAllIngredient(pagination).then(ingredients => {
             res
                 .status(statusCode.OK)
                 .json(success("OK", ingredients.data, statusCode.OK, pagination.header, ingredients.totalPage));
@@ -41,7 +41,7 @@ const updateIngredient = (req, res) => {
         var body = req.body;
         var id = body.params.id;
 
-        Validator.ValidateId(id, "El id de la receta es inválido");
+        Validator.ValidateId(id, "El id del ingrediente es inválido");
 
         return ingredientService.updateIngredient(id, body).then(updatedRow => {
             if (updatedRow == null) {
@@ -64,9 +64,9 @@ const deleteIngredient = (req, res) => {
         var body = req.body;
         var id = req.params.id;
 
-        Validator.ValidateId(recipeId, "El id del ingrediente es inválido");
+        Validator.ValidateId(id, "El id del ingrediente es inválido");
 
-        return ingredientService.deleteIngredient(recipeId, body).then(deletedRow => {
+        return ingredientService.deleteIngredient(id, body).then(deletedRow => {
             if (deletedRow == null) {
                 res
                     .status(statusCode.OK)
