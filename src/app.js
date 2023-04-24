8c5fd9eab5759cfaeb33e69a766873ff15980e37
EstebanMadrazo <3steban.madrazo@gmail.com>
2023-04-24 04:00:53 -0600

    Added ingredient

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const v1CommentRouter = require('./v1/routes/commentRoutes');
 const v1RecipeRouter = require('./v1/routes/recipeRoutes');
 const v1UserRouter = require("./v1/routes/userRoutes");
+const v1IngredientRouter = require("./v1/routes/ingredientRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/api/v1/comment", v1CommentRouter);
 app.use("/api/v1/recipe", v1RecipeRouter);
 app.use("/api/v1/user", v1UserRouter);
+app.use("/api/v1/ingredient", v1IngredientRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
