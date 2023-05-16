6659e096a238736455071cfeef488eaa6ecddcfe
Alexandra Oreza <A18016321@alumnos.uady.mx>
2023-05-16 17:37:45 -0600

    comment for ingredientService

--- src/services/ingredientService.js
+++ src/services/ingredientService.js
@@ -1,5 +1,11 @@
 const Ingredient = require('../database/models/ingredient');
 
+/**
+ * Crea un nuevo ingrediente.
+ * 
+ * @param {*} req : Información del request: info del ingrediente 
+ * @returns una promesa que se resuelve con el ingrediente creado
+ */
 const postIngredient = (req) => {
     return Ingredient.create({
         id: req.id,
@@ -9,6 +15,11 @@ const postIngredient = (req) => {
     })
 }
 
+/**
+ * Obtiene todos los ingredientes paginados
+ * @param {*} pagination : Configuración de la paginación
+ * @returns una promesa que se resuelve con los ingredientes obtenidos
+ */
 const getAllIngredient = async (pagination) => {
     const amount = await Ingredient.count();
 
@@ -21,6 +32,12 @@ const getAllIngredient = async (pagination) => {
     });
 }
 
+/**
+ * Obtiene un ingrediente de acuerdo con el Id provisto
+ * 
+ * @param {*} ingredientId : Identificador del ingrediente a buscar
+ * @returns una promesa que se resuelve con la identidicación del ingrediente encontrado
+ */
 const getIngredientById = (ingredientId) => {
     return Ingredient.findOne({
         where: {
